f92cc9e207eebcbb51aca47be0cb6914d335ce31
andrus1st <andrus1@gmail.com>
2021-03-08 03:39:25 -0300

    rutes completadas

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 const productsController = require("../controllers/productsController");
 
+// Listar productos
 router.get('/', productsController.index);
+
+// Detalle de un producto
+router.get('/:id', productsController.showProduct);
+
+// Crear productos
+router.get('/create', productsController.createForm);
+router.post("/", productsController.createNewProduct);
 
 // Editar un producto
 router.get('/edit/:id', productsController.edit);
