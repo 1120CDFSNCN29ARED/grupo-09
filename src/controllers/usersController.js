0efdf05576583b0d52edec9ee306ef87a3d04275
andrus1st <andrus1@gmail.com>
2021-04-08 21:01:50 -0300

    implementacion usuarios

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,39 @@
+//const { restart } = require("nodemon");
+const fs = require("fs");
+const path = require("path");
+
+const usersFilePath = path.resolve(__dirname, "../../Users.json");
+const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+
 const controlador = {
-    index: (req, res) => {
-    res.render("editProduct");
-    },
-}
+  index: (req, res) => {
+    const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+    res.render("users", { users });
+  },
+  
+    loginForm: (req, res) => {
+    res.render("login");
+  },
+
+  registrationForm: (req, res) => {
+    res.render("registration");
+  },
+
+  registration: (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+  let newUser = req.body;
+  newUser.image = req.file.filename;
+  let greatestId = 0;
+  users.map((user) => {
+    user.id > greatestId ? (greatestId = user.id) : "";
+  });
+  newUser.id = greatestId + 1;
+  users.push(newUser);
+  fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 4));
+  console.log("********* CREATION SUCCESSFUL **************");
+  res.redirect("/users");
+  },
+};
 
 module.exports = controlador;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+
 const usersController = require("../controllers/usersController");
 
-router.get("/login", usersController.index);
-router.get("/register", usersController.index);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/img/users");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+const uploadFile = multer({ storage });
+
+router.get("/login", usersController.loginForm);
+router.get("/register", usersController.registrationForm);
+router.get("/", usersController.index);
+router.post("/", uploadFile.single("image"), usersController.registration);
 
 module.exports = router;
