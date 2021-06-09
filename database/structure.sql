e07f5bc1b030ef9cebb37a4a1b0e0f615060a102
solaguilera <solaguilera@pm.me>
2021-06-08 21:06:04 -0300

    sql file updated

--- database/structure.sql
+++ database/structure.sql
@@ -1,31 +1,35 @@
 USE AMapsDb;
 
 CREATE TABLE `Users` (
-        `UserId` INT UNSIGNED NOT NULL AUTO_INCREMENT FOREIGN KEY,
+        `UserId` INT UNSIGNED AUTO_INCREMENT FOREIGN KEY,
         `Names` VARCHAR(50) NOT NULL,
         `Email` VARCHAR(50) NOT NULL,
         `Address` VARCHAR(50) NOT NULL,
         `Password` VARCHAR(50) NOT NULL,
         `Image` VARCHAR(100) DEFAULT NULL,
-    PRIMARY KEY (`Id`)
+    PRIMARY KEY (`UserId`)
 )
 
 CREATE TABLE `Products` (
-        `ProductId` INT UNSIGNED NOT NULL AUTO_INCREMENT FOREIGN KEY,
+        `ProductId` INT UNSIGNED AUTO_INCREMENT FOREIGN KEY,
         `Name` VARCHAR(50) NOT NULL,
         `Continent` VARCHAR(50),
         `Description` TEXT NOT NULL,
         `UnitPrice` DECIMAL(10, 2),
         `Image` VARCHAR(100) NOT NULL,
         `Stock` INT,
-    PRIMARY KEY (`Id`)
+
+    PRIMARY KEY (`ProductId`),
 )
 
 CREATE TABLE `Order` (
-        `OrderId` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        `UserId` FOREIGN KEY,
-        `ProductId` FOREIGN KEY,
+        `OrderId` INT UNSIGNED AUTO_INCREMENT,
+        `UserId` INT,
+        `ProductId` INT,
         `Quantity` INT UNSIGNED NOT NULL,
         `Price` DECIMAL(10, 2),
     PRIMARY KEY (`OrderId`)
+    FOREIGN KEY (`ProductId`) REFERENCES `Products`(`ProductId`),
+    FOREIGN KEY (`UserId`) REFERENCES `Users`(`UserId`)
 )
+ 
