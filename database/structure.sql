bed651ddf03e799f73d9f18d5ef56ee0bc8417ea
andrus1st <andrus1@gmail.com>
2021-06-08 22:19:23 -0300

    two working tables

--- database/structure.sql
+++ database/structure.sql
@@ -1,35 +1,23 @@
-USE AMapsDb;
+use AMapsDb;
+drop table if exists Users;
+create table Users (
+Id int unsigned auto_increment,
+Names varchar(50) not null,
+Email varchar(50) not null,
+Address varchar(50) not null,
+Password varchar(50) not null,
+Image varchar(100),
+primary key (Id)
+);
 
-CREATE TABLE `Users` (
-        `UserId` INT UNSIGNED AUTO_INCREMENT FOREIGN KEY,
-        `Names` VARCHAR(50) NOT NULL,
-        `Email` VARCHAR(50) NOT NULL,
-        `Address` VARCHAR(50) NOT NULL,
-        `Password` VARCHAR(50) NOT NULL,
-        `Image` VARCHAR(100) DEFAULT NULL,
-    PRIMARY KEY (`UserId`)
-)
-
-CREATE TABLE `Products` (
-        `ProductId` INT UNSIGNED AUTO_INCREMENT FOREIGN KEY,
-        `Name` VARCHAR(50) NOT NULL,
-        `Continent` VARCHAR(50),
-        `Description` TEXT NOT NULL,
-        `UnitPrice` DECIMAL(10, 2),
-        `Image` VARCHAR(100) NOT NULL,
-        `Stock` INT,
-
-    PRIMARY KEY (`ProductId`),
-)
-
-CREATE TABLE `Order` (
-        `OrderId` INT UNSIGNED AUTO_INCREMENT,
-        `UserId` INT,
-        `ProductId` INT,
-        `Quantity` INT UNSIGNED NOT NULL,
-        `Price` DECIMAL(10, 2),
-    PRIMARY KEY (`OrderId`)
-    FOREIGN KEY (`ProductId`) REFERENCES `Products`(`ProductId`),
-    FOREIGN KEY (`UserId`) REFERENCES `Users`(`UserId`)
-)
- 
+drop table if exists Products;
+create table Products (
+Id int unsigned auto_increment,
+Name varchar(50) not null,
+Continent varchar(50),
+Description text not null,
+UnitPrice decimal(10, 2),
+Image varchar(100) not null,
+Stock int,
+primary key(Id)
+);
