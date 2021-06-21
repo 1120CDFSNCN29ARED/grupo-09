847e1517e75820162cc74181d72dffa2b72111a6
solaguilera <solaguilera@pm.me>
2021-06-21 19:49:36 -0300

    merge

--- database/config/config.js
+++ database/config/config.js
@@ -2,33 +2,21 @@ module.exports = {
   development: {
     username: "root",
     password: null,
-<<<<<<< HEAD
     database: "AMapsDb",
-=======
-    database: "amapsdb",
->>>>>>> origin
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
     username: "root",
     password: null,
-<<<<<<< HEAD
     database: "AMapsDb",
-=======
-    database: "amapsdb",
->>>>>>> origin
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
     username: "root",
     password: null,
-<<<<<<< HEAD
     database: "AMapsDb",
-=======
-    database: "amapsdb",
->>>>>>> origin
     host: "127.0.0.1",
     dialect: "mysql",
   },
