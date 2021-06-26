8d267d1c821ee4c1652fe4a6648b87c27c44b0c4
solaguilera <solaguilera@pm.me>
2021-06-26 13:07:02 -0300

    working form val

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,5 +1,5 @@
-window.addEventListener("load", function(){
-    
+window.addEventListener("load", function () {
+
     let RegistrationForm = document.querySelector("#reg-form");
     let ul = document.querySelector("#err-front-validation");
     let names = document.querySelector("#names");
@@ -7,41 +7,41 @@ window.addEventListener("load", function(){
     let address = document.querySelector("#address");
     let password = document.querySelector("#password");
     let err = [];
-    let errorCheck = function(msg) {
+    let errorCheck = function (msg) {
         err.indexOf(msg) == -1 ? err.push(msg) : '';
     }
 
-     RegistrationForm.addEventListener("submit", function(e){
+    RegistrationForm.addEventListener("submit", function (e) {
 
-         err = [];
-         err.length == 0 ? ul.innerHTML = '' : '';
-
-        if (names.value == ""){
+        if (names.value == "") {
             errorCheck("El campo de nombres debe completarse");
         } else if (names.value.length < 2) {
             errorCheck("El campo de nombres debe tener al menos 2 caracteres");
         };
-
+        
         if (email.value == "") {
             errorCheck("Debe ingresar un e-mail");
         } else if (!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(email.value)) {
             errorCheck("El e-mail no es válido");
         };
-
-        if (address.value == ""){
+        
+        if (address.value == "") {
             errorCheck("Debe ingresar una dirección");
         };
-
+        
         if (password.value == "") {
             errorCheck("El campo de constraseña es obligatorio");
-        }else if (password.value.length < 8) {
+        } else if (password.value.length < 8) {
             errorCheck("La contraseña debe tener al menos 8 caracteres");
         };
-
+        
+        
         if (err.length > 0) {
             e.preventDefault();
-            err.forEach( unError => ul.innerHTML += '<li>' + unError + '</li>');
-        };    
+            ul.innerHTML = '';
+            err.forEach(unError => ul.innerHTML += '<li>' + unError + '</li>');
+        };
+        err = [];
 
-    });    
+    });
 });
