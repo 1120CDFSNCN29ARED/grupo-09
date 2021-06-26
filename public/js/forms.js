9cf7f8449bb0d7d46108838e00acd8a152d5d895
solaguilera <solaguilera@pm.me>
2021-06-26 12:14:09 -0300

    forms.js update

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,47 +1,47 @@
 window.addEventListener("load", function(){
     
-    let RegistrationForm = document.querySelector(".registration-form");
-    let ulerrores = document.querySelector("#errores-front-validation");
-    let campoNombre = document.querySelector("#names");
-    let campoEmail = document.querySelector("#email");
-    let campoDireccion = document.querySelector("#address");
-    let campoConstrasenia = document.querySelector("#password");
-    let errores = [];
-    let erroresCheck = function(msg) {
-        errores.indexOf(msg) == -1 ? errores.push(msg) : '';
+    let RegistrationForm = document.querySelector("#reg-form");
+    let ul = document.querySelector("#err-front-validation");
+    let names = document.querySelector("#names");
+    let email = document.querySelector("#email");
+    let address = document.querySelector("#address");
+    let password = document.querySelector("#password");
+    let err = [];
+    let errorCheck = function(msg) {
+        err.indexOf(msg) == -1 ? err.push(msg) : '';
     }
+
      RegistrationForm.addEventListener("submit", function(e){
-        console.log("ERR!!!", errores)
-         errores = [];
-        if (campoNombre.value == ""){
-            erroresCheck("El campo de nombres debe completarse");
-        }else if(campoNombre.value.length < 2){
-            erroresCheck("El campo de nombres debe tener al menos 2 caracteres");
-        };
-        if (campoEmail.value == ""){
-            erroresCheck("Debe ingresar un e-mail");
-        } else if (!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(campoEmail.value)) {
-            erroresCheck("El e-mail no es válido");
+
+         err = [];
+         err.length == 0 ? ul.innerHTML = '' : '';
+
+        if (names.value == ""){
+            errorCheck("El campo de nombres debe completarse");
+        } else if (names.value.length < 2) {
+            errorCheck("El campo de nombres debe tener al menos 2 caracteres");
         };
 
-        if (campoDireccion.value == ""){
-            erroresCheck("Debe ingresar una dirección");
-        }
+        if (email.value == "") {
+            errorCheck("Debe ingresar un e-mail");
+        } else if (!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(email.value)) {
+            errorCheck("El e-mail no es válido");
+        };
 
-        if (campoConstrasenia.value == ""){
-            erroresCheck("El campo de constraseña es obligatorio");
-        }else if (campoConstrasenia.value.length < 8) {
-            erroresCheck("La contraseña debe tener al menos 8 caracteres");
-        }
+        if (address.value == ""){
+            errorCheck("Debe ingresar una dirección");
+        };
 
-        if (errores.length > 0){
+        if (password.value == "") {
+            errorCheck("El campo de constraseña es obligatorio");
+        }else if (password.value.length < 8) {
+            errorCheck("La contraseña debe tener al menos 8 caracteres");
+        };
+
+        if (err.length > 0) {
             e.preventDefault();
-            errores.forEach( unError => ulerrores.innerHTML += '<li>' + unError + '</li>');
-        } 
-        
-        
-    }
-    
-    );
-    
+            err.forEach( unError => ul.innerHTML += '<li>' + unError + '</li>');
+        };    
+
+    });    
 });
